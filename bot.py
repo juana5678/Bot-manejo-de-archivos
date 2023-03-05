a7b50e864fc0c4b6d939100d0964ffb262097299
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-05 16:16:36 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,6 @@ import tgcrypto
 import os
 from pyrogram import Client , filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from os.path import exists
-import shutil
-import asyncio
-from os import mkdir
-from os import unlink
-import platform
 
 
 #BoT Configuration Variables
@@ -15,7 +9,7 @@ api_id = 9910861
 api_hash = "86e927460a8998ba6d84e9c13acfda95"
 bot_token = "6281846554:AAEQ97AM-d289ADS-bhJdAEpBvnHhF2crYY"
 Channel_Id = -1001804018431
-app = Client("bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
+bot = Client("bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
 boss = ['UHTRED_OF_BEBBANBURG','Stvz20']#usuarios supremos
 Configs = {"uclv":'',"gtm":"","uvs":"","ltu":"a816210ff41853b689c154bad264da8e", 
 			"ucuser": "", "ucpass":"","uclv_p":"", "gp":'socks5://190.15.159.152:10089', "s":"On", 
@@ -34,10 +28,10 @@ downlist = {} #lista de archivos descargados
 procesos = 0 #numero de procesos activos en el bot
 
 #inicio
-@app.on_message(filters.private)
-async def hello(client, message):
-    await message.reply("Hello from Pyrogram!")
+@bot.on_message(filters.command("start", prefixes="/") & filters.private)
+async def start(client, message):
+    await message.reply("A Presionado el Botón ⏭️")
 
-app.start()
-app.send_message(5416296262,'**BoT Iniciado**')
-app.loop.run_forever()
+bot.start()
+bot.send_message(5416296262,'**BoT Iniciado**')
+bot.loop.run_forever()
