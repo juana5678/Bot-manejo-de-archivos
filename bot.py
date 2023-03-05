3560f39f02e913f7f1cfe29b5831134625d01ae9
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-05 17:03:54 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,19 +29,7 @@ procesos = 0 #numero de procesos activos en el bot
 
 #Acceso de Uso
 def comprobacion_de_user(username):
-    if username in Config or username in boss:pass		
-	if exists('downloads/'+str(username)+'/'):pass
-	else:os.makedirs('downloads/'+str(username)+'/')	
-	try:Urls[username]
-	except:Urls[username] = []
-	try:Config[username]
-	except:Config[username] = {"username":"","password":"","repoid":"","host":""}
-	try:id_de_ms[username]
-	except:id_de_ms[username] = {"msg":"","proc":""}
-	try:root[username]
-	except:root[username] = {"actual_root":f"downloads/{str(username)}"}
-	try:downlist[username]
-	except:downlist[username] = []
+    if username in Config or username in boss:pass
     else:
 	return False
 
