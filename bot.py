c4e944f89b851e008780645b0e8d778c9f215c27
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-06 19:54:35 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -883,8 +883,7 @@ async def uploadfile(file,usid,msg,username):
             while logerrors < 5:
                 error_conv = 0
                 try:
-                    upload = await client.uploadtoken(path,lambda chunk,total,start,filen:
-                    uploadfile_progres(chunk,total,start,filen,msg),token)
+                    upload = await client.uploadtoken(path,lambda chunk,total,start,filen:uploadfile_progres(chunk,total,start,filen,msg),token)
                     if mode == "upltu" or mode == "upgtm" or mode == "upcmw":
                         upload = upload[1]
                         upload = upload.replace('draftfile.php/','webservice/draftfile.php/')
