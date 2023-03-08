f719fc1e6800953e16ff2b8c1946933c718f51de
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-08 00:50:09 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ from multivolumefile import MultiVolume
 #BoT Configuration Variables
 api_id = 9910861
 api_hash = "86e927460a8998ba6d84e9c13acfda95"
-bot_token = "6046319366:AAEAvV_YoQj1tgn3VlhbLefiI01WJ8aKJ4I"
+bot_token = os.environ.get('bot_token')
 Channel_Id = -1001804018431
 bot = Client("bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
 boss = ['UHTRED_OF_BEBBANBURG','Stvz20']#usuarios supremos
