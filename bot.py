7ab949103837c90315e049a595e66d3ea059385d
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-13 01:08:46 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,12 +64,34 @@ downlist = {} #lista de archivos descargados
 procesos = 0 #numero de procesos activos en el bot
 
 ###Buttons
-@bot.on_message(filters.command('/up_time') & filters.private)
+@bot.on_message(filters.command('/time') & filters.private)
 async def up_time(bot, message):
     uptime = get_readable_time(time() - start)
     username = message.from_user.username
     await bot.send_message(username, "uptime: {uptime}")
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    readable_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", " days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        readable_time += time_list.pop() + ", "
+    time_list.reverse()
+    readable_time += ": ".join(time_list)
+    return readable_time
 
 #Funcion
 seg = 0
