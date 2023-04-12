dff04c3a7aa7bb7300027490dd465b1c7f5db667
juana5678 <118631590+juana5678@users.noreply.github.com>
2023-04-11 21:49:05 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ import NexCloudClient
 #BoT Configuration Variables
 api_id = 10181262
 api_hash = "f52b5a057b73b9974eaa7403e04907f0"
-bot_token = os.environ.get('5947045568:AAE0mfkt85dPRAYIKGBEFdHd4qG4U3Y4bAA')
+bot_token ='5947045568:AAE0mfkt85dPRAYIKGBEFdHd4qG4U3Y4bAA'
 Channel_Id = -1001648620646
 msg_id = 3
 bot = Client("bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
